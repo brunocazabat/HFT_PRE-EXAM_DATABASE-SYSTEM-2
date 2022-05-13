604f57a2180efea59bcfb6a796a4e10800789cb2
bruno cazabat <bruno.cazabat@epitech.eu>
2022-05-13 10:22:27 +0200

    Update table_injector.py

--- table_injector.py
+++ table_injector.py
@@ -2,6 +2,7 @@ import psycopg2
 
 try:
     conn = psycopg2.connect("host='localhost' port = '5432' dbname='dbpizzaprojecthft' user='bruno' password='admin'")
+    print("Successfully connected to the Pizza PostgreSQL DataBase.")
 except:
     print("I am unable to connect to the database")
 
@@ -9,8 +10,10 @@ curs = conn.cursor()
 
 try:
     curs.execute(open('pizza_schema.sql', 'r').read())
+    print("Successfully Injected basic Pizzas Recipes into the PostgreSQL DataBase.")
 except:
-    print("I can't drop our test database!")
+    print("I can't drop our Pizza database! Maybe Pizzas are already here ..")
 
 conn.close()
+print("Closing Table_injector.py")
 curs.close()
